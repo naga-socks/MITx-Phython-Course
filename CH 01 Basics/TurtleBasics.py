b372685cb909ea15c55b883603846ca633a671e7
Jhonatan Nagasako <jhonatannagasako@gmail.com>
2018-12-05 08:19:20 -0500

    Uploading basic test files and for loop example

---
+++ CH 01 Basics/TurtleBasics.py
@@ -0,0 +1,9 @@
+import turtle
+myTurtle = turtle.Turtle()
+myTurtle.forward(100)
+myTurtle.right(90)
+myTurtle.forward(100)
+myTurtle.right(90)
+myTurtle.forward(100)
+myTurtle.right(90)
+myTurtle.forward(100)
